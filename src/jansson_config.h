6d4b68534febc002e14bc91353e1f626dad2c0b0
amirparto1 <amirparto24@yahoo.com>
2021-10-26 13:49:16 +0330

    JSON_PARSER_MAX_DEPTH 3072

--- src/jansson_config.h
+++ src/jansson_config.h
@@ -46,6 +46,6 @@
 
 /* Maximum recursion depth for parsing JSON input.
    This limits the depth of e.g. array-within-array constructions. */
-#define JSON_PARSER_MAX_DEPTH 2048
+#define JSON_PARSER_MAX_DEPTH 3072
 
 #endif
